f3533b0c39ca3ea5ad0a72102430438890b8d495
Areaefred <weiheng@weihengdeMacBook-Pro.local>
2014-08-10 01:19:34 +0800

    0810-4
    删除空格

--- Nurse.m
+++ Nurse.m
@@ -13,35 +13,24 @@
 -(void)takeEat
 
 {
-    
     NSLog(@"%小孩饿了，喂它吃饭");
-    
 }
 
 -(void)takeSleep
 
 {
-    
     NSLog(@"小孩困了，哄他睡觉");
-    
 }
 
 -(void)takePlay
-
 {
-    
     NSLog(@"小孩醒来了，陪他玩");
-    
 }
 
 -(void)takeShower
-
 {
-    
     NSLog(@"晚上给小孩洗澡");
-    
 }
-
 
 
 @end
