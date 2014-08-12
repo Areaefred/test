27ca4df48ee0979c69d386825b13b59bd2c77518
Areaefred <weiheng@weihengdeMacBook-Pro.local>
2014-08-12 13:19:33 +0800

    知识积累

---
+++ BasicKnowledgeAccumulation.h
@@ -0,0 +1,13 @@
+//
+//  BasicKnowledgeAccumulation.h
+//  TEST0812
+//
+//  Created by weiheng on 14-8-12.
+//  Copyright (c) 2014年 weiheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BasicKnowledgeAccumulation : NSData
+
+@end
---
+++ BasicKnowledgeAccumulation.m
@@ -0,0 +1,33 @@
+//
+//  BasicKnowledgeAccumulation.m
+//  TEST0812
+//
+//  Created by weiheng on 14-8-12.
+//  Copyright (c) 2014年 weiheng. All rights reserved.
+//
+
+#import "BasicKnowledgeAccumulation.h"
+
+@implementation BasicKnowledgeAccumulation
+
+@end
+/*
+ NSMutableDictionary的使用方法
+ @property(nonatomic,strong)NSMutableDictionary* dataSourceDic;
+ self.dataSourceDic=[NSMutableDictionary dictionaryWithCapacity:2];
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
