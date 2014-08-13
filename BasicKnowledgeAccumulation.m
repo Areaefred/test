b01a713dbb988b4523079ea3dcb2fd1aa5b772ee
Areaefred <weiheng@weihengdeMacBook-Pro.local>
2014-08-13 08:44:53 +0800

    每日积累
    NSMutableDictionary的使用方法+生成固定大小数据的方法

--- BasicKnowledgeAccumulation.m
+++ BasicKnowledgeAccumulation.m
@@ -12,22 +12,35 @@
 
 @end
 /*
- NSMutableDictionary的使用方法
+ NSMutableDictionary的使用方法 NSString
+ 
+ 
  @property(nonatomic,strong)NSMutableDictionary* dataSourceDic;
  self.dataSourceDic=[NSMutableDictionary dictionaryWithCapacity:2];
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
  */
+
+ /*
+ 生成1024*1024*2的数据以测试APP间通过URL方式转递数据的大小
+ unsigned char* pemdata = NULL;
+ unsigned long pemdatalen = 1024*1024;
+ pemdata = (unsigned char*)malloc(pemdatalen);
+ memset(pemdata, 0x65, pemdatalen);
+ NSString * strTmp = @"";
+ for (int i=0; i<pemdatalen; i++) {
+ strTmp = [NSString stringWithFormat:@"%@%@",strTmp,[NSString stringWithFormat:@"%02x",pemdata[i]&0xff]];
+ }
+ free(pemdata);
+ NSLog(@"strTmp=%@",strTmp);
+ */
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
