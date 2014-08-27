287010ef500d5df09127f1a312ec12670f84840d
Areaefred <weiheng@weihengdeMacBook-Pro.local>
2014-08-27 10:57:24 +0800

    知识积累
    UILabel、UIButton、UIAlertView、UIAlertViewDelegate方法、圆角、strong、weak

--- BasicKnowledgeAccumulation.m
+++ BasicKnowledgeAccumulation.m
@@ -16,7 +16,46 @@
  @property(nonatomic,strong)NSMutableDictionary* dataSourceDic;
  self.dataSourceDic=[NSMutableDictionary dictionaryWithCapacity:2];
  */
+/*
+ 
+ 
+    UILabel *label;
 
+
+    label=[[UILabel alloc]init];
+    label.text=@"第二页";
+    label.frame=CGRectMake(135, 20, 60, 40);
+    [self.view addSubview:label];
+
+
+
+    label=[[UILabel alloc]init];
+    label.text=@"本页由代码编写";
+    label.frame=CGRectMake(110, 70, 120, 40);
+    [self.view addSubview:label];
+ 
+ 
+    UIButton *btn;
+    btn=[[UIButton alloc]init];
+    [btn setTitle:@"测试二维数组" forState:UIControlStateNormal] ;
+    btn.frame=CGRectMake(20, 120, 150, 30);
+    [self.view addSubview:btn];
+    [btn addTarget:self action:@selector(btn1do:) forControlEvents:UIControlEventTouchUpInside];
+
+    btn=[UIButton buttonWithType:UIButtonTypeCustom];
+    btn.backgroundColor=[UIColor whiteColor];
+    [btn.layer setCornerRadius:5];
+ 
+    [btn setTitle:@"返回上一页" forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor blueColor] forState:UIControlStateHighlighted];
+
+    //btn setAttributedTitle:<#(NSAttributedString *)#> forState:<#(UIControlState)#>
+    btn.tintColor=[UIColor blackColor];
+    btn.frame=CGRectMake(100, 420, 120, 30);
+    [self.view addSubview:btn];
+    [btn addTarget:self action:@selector(btn2do:) forControlEvents:UIControlEventTouchUpInside];
+*/
  /*
  //生成1024*1024*2的数据以测试APP间通过URL方式转递数据的大小
  unsigned char* pemdata = NULL;
@@ -33,13 +72,55 @@
  
  /*
  
-UIAlertView *alert=[[UIAlertView alloc] initWithTitle:@"第一次"
-                                              message:@"进入App"
-                                             delegate:self
-                                    cancelButtonTitle:@"我知道了"
-                                    otherButtonTitles:nil];
+UIAlertView *alert=[[UIAlertView alloc] initWithTitle:@"标题" message:@"内容"  delegate:self  cancelButtonTitle:@"确定" otherButtonTitles:nil];
+  [alert show];
+  //设置tag
+  alert.tag==2;
+  
+  // 设置显示样式
+  [alert setAlertViewStyle:UIAlertViewStylePlainTextInput];//为明文
+  
+  UITextField *textField = [alert textFieldAtIndex:0];注意取出来 就可以操作alert的该项
+  
+  // 设置输入键盘样式
+  //textField.keyboardType = UIKeyboardTypeNumberPad;//设定某个输入框的输入键盘格式
+  //textField.keyboardType = UIKeyboardTypeNumbersAndPunctuation;//设定输入键盘格式
+  textField.keyboardType = UIKeyboardTypeASCIICapable;
+  
 [alert show];
 
+  
+  [self.tv1.layer setCornerRadius:10];
+  
+  //设置按钮等控件的圆角
+  [self.ChangeUsrPwd.layer setCornerRadius:10];
+  [self.LogOut.layer setCornerRadius:10];
+  
+  
+  
+
+  
+  
+  #pragma mark  代理UIAlertViewDelegate方法，判断那个按钮被点击
+  - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+  {
+  // 获得当前索引按钮的标题
+  NSString *buttonTitle = [alertView buttonTitleAtIndex:buttonIndex];
+  // 根据标题判断那个按钮被单击
+  if ([buttonTitle isEqualToString:@"Cancel"]||[buttonTitle isEqualToString:@"取消"])
+  {
+  //[self.view removeFromSuperview];
+  return;
+  }
+  else if([buttonTitle isEqualToString:@"确定"])
+  {
+  // 获得索引为0的UITextField
+  UITextField *textField = [alertView textFieldAtIndex:0];
+  }
+  }
+  }
+  
+  
 */
 
 
--- test/FirstViewController.h
+++ test/FirstViewController.h
@@ -9,10 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface FirstViewController : UIViewController
-@property (weak, nonatomic) IBOutlet UITextView *UItextView_Time;
+@property (weak, nonatomic) IBOutlet UITextView *UItextView_Time;//和nib控件结合可以使用weak方式
 - (IBAction)UIButton_update:(id)sender;
 - (void)timerAction:(NSTimer*)timer;
 @property (weak, nonatomic) IBOutlet UIButton *UIButton_Update;
 @property (weak, nonatomic) IBOutlet UIButton *UIButton_Stop;
 - (IBAction)UIButton_TouchUpInside:(id)sender;
+@property (strong, nonatomic) IBOutlet UILabel *positionLabel;//不和nib控件结合，要使用strong方式
 @end
